0ba1f667eedf8f0cb4fd3a58df4023794de6ddbb
Sukucee <magabokc@gmail.com>
2024-09-24 02:22:24 +0800

    #wordreverse

---
+++ src/app/word-reverser/word-reverser.component.ts
@@ -0,0 +1,25 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-word-reverser',
+  standalone: true,
+  imports: [],
+  templateUrl: './word-reverser.component.html',
+  styleUrl: './word-reverser.component.css'
+})
+export class WordReverserComponent {
+  @ViewChild('inputWord') inputWord!: ElementRef;
+    Result = '';
+
+    reverseTheWord(): void {
+        this.Result = this.reverser(this.inputWord.nativeElement.value);
+    }
+
+    reverser(str: string): string {
+        let reversedWord = '';
+        for (let i = str.length - 1; i >= 0; i--) {
+            reversedWord += str[i];
+        }
+        return reversedWord;
+    }
+}
